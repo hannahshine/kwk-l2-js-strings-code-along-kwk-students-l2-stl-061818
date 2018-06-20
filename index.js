c8986d014f360958f6b594d2839ee2cfd982cace
Hannah Shine <hshine@visitationacademy.org>
2018-06-20 20:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser='Hannah Shine';
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`; 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = ... ;
