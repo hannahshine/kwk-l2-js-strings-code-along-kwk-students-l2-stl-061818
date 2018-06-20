da7984f022ddcbd82f22be589d288a20167c6008
Hannah Shine <hshine@visitationacademy.org>
2018-06-20 20:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser='Hannah Shine';
-const welcomeMessage = 'Welcome to Klossybook, currentUser';
+const welcomeMessage = 'Welcome to Klossybook' + 'currentUser';
